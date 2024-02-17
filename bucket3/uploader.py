fad2dcc442aaaf21a0185a55f49715dc6f2a8c34
Zhuoyun Wei <wzyboy@wzyboy.org>
2024-02-17 13:06:35 -0800

    feat: backend API for upload form data

--- bucket3/uploader.py
+++ bucket3/uploader.py
@@ -53,3 +53,39 @@ class Uploader:
         # Key should already be URL-safe. Invoke quote() to ensure this.
         url = f'https://{self.domain}/{quote(key)}'
         return url
+
+    def get_upload_form_data(self, key: str) -> dict:
+        '''Request a presigned POST from AWS. The frontend uses the data to
+        build a web form to upload the file directly to AWS.'''
+        # https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-UsingHTTPPOST.html
+
+        # The frontend calculates the checksum and derive the key from it. Here
+        # we convert the key back to the checksum and attach it to the request
+        # so that AWS can verify the integrity of the file.
+        checksum = Hasher.from_key(key).checksum()
+        fields = {
+            'x-amz-checksum-algorithm': 'SHA256',
+            'x-amz-checksum-sha256': checksum,
+        }
+
+        # Limit what can be done with the generated presigned request.
+        # https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-HTTPPOSTConstructPolicy.html
+        # These conditions are signed against so they cannot be tempered by the
+        # frontend. Boto3 already includes `bucket` and `key` in the conditions,
+        # but it won't automatically add custom fields added above.
+        conditions = [
+            ["starts-with", "$Content-Type", "image/"],
+            {'x-amz-checksum-algorithm': 'SHA256'},
+            {'x-amz-checksum-sha256': checksum},
+        ]
+
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/generate_presigned_post.html
+        response = self.s3.generate_presigned_post(
+            self.bucket,
+            key,
+            Fields=fields,
+            Conditions=conditions,
+        )
+
+        # The response contains the presigned URL and required fields
+        return response
